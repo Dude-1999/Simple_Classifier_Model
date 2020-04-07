9345675f8688f267dd71ad6068c506e1cbd3e81c
Kausthub8 <47001638+Dude-1999@users.noreply.github.com>
2020-04-07 20:47:05 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,21 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+target = [1 if i < 12500 else 0 for i in range(25000)]
+
+X_train, X_val, y_train, y_val = train_test_split(
+    X, target, train_size = 0.75
+)
+
+for c in [0.01, 0.25, 0.5, 1]:
+    
+    lr = LogisticRegression(C=c)
+    lr.fit(X_train, y_train)
+    print ("Accuracy for C=%s: %s" 
+           % (c, accuracy_score(y_val, lr.predict(X_val))))
+
+final_model = LogisticRegression(C=0.05)
+final_model.fit(X, target)
+print ("Final Accuracy: %s" 
+       % accuracy_score(target, final_model.predict(X_test)))
